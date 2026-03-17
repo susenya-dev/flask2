7c60013b74d209413c0ab4a1b69cfd6e30dd3ecd
susenya-dev <s8850171@gmail.com>
2026-03-17 12:22:28 +0300

    first

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+from data import db_session
+from data.jobs import Jobs
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+@app.route('/index/')
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return render_template("index.html", jobs=jobs)
+
+
+def main():
+    db_session.global_init("db/mars_explorer.db")
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
